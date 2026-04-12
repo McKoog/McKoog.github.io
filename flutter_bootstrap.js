6164a09210ebda8bb933781fcb7fe0b332a99ef2
McKoog <koliandru@mail.ru>
2026-04-12 15:35:40 +0700

    Updated release 0.1.8 enchantment game

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "501897369" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "560290727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
